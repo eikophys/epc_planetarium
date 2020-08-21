c78f70e386f12f8ce4249ca392a0e3ccd849445a
apppare <kaibatsu35.7m45@gmail.com>
2020-08-21 10:48:34 +0900

    Set style
    リサイズ処理などスタイルを設定

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Three.jsを用いたプラネタリウムを作っています
 
 ## Usage
 - [Three.js](https://threejs.org) 
+libsフォルダーにThree jsのJSファイルを保存します
 
 ## Licence
 [MIT Licence](https://github.com/appare45/Planetarium/blob/master/LICENCE)
--- js/main.js
+++ js/main.js
@@ -8,35 +8,41 @@ const init = () => {
   // カメラ
   const camera = new THREE.PerspectiveCamera(45, width / height);
   camera.position.set(0, 0, 1000);
-  scene.add(camera)
+  scene.add(camera);
   
   // レンダラー
   const renderer = new THREE.WebGLRenderer({ antialias:true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
-  canvas.appendChild(renderer.domElement)
+  canvas.appendChild(renderer.domElement);
   
 
-  // ドーナツを作成
-  const geometry = new THREE.SphereGeometry(300, 30, 30);
-  // マテリアルを作成
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
-  // メッシュを作成
-  const mesh = new THREE.Mesh(geometry, material);
-  // 3D空間にメッシュを追加;
-  scene.add(mesh);
-  // 平行光源
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(directionalLight);
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(400, 400, 400);
+  const material = new THREE.MeshNormalMaterial();
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);// シーンに追加
 
 
   // フレーム更新
   const tick = () => {
+    box.rotation.y += 0.01;
     renderer.render(scene, camera)
     requestAnimationFrame(tick)
   }
   
   tick();
+
+  // リサイズ
+  window.addEventListener('resize', () => {
+    // サイズの取得
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  })
 }
